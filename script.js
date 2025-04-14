76307247aa0eafabc15a8e4a3cc584d7a6a98a33
sarahcgraham <64796230+sarahcgraham@users.noreply.github.com>
2025-04-13 19:27:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Mobile menu toggle functionality
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.getElementById('nav-menu');
-
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('open');
-});
-
-document.querySelectorAll('.nav-menu a').forEach(link => {
-  link.addEventListener('click', () => {
-    navMenu.classList.remove('open');
-  });
-});
+// Sample JS placeholder
